5c81545ea42d5a832fbac5a500ec47c42d7948d0
ZakhonaNdwalane <zakhona.chiliza@gmail.com>
2025-01-12 16:38:33 +0200

    Fixed the Icon and Time dipslay

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.city;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#windspeed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#image");
 
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  timeElement.innerHTML = formatDay(date);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
 }
 
+function formatDay(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apikey = "c4d9be47o0b370b37f28te5a42babf6c";
   const apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
